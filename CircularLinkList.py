1531261c0562b9210dc6b6c6c45c8aff1034a495
riteshvado <87545202+riteshvado@users.noreply.github.com>
2021-09-11 08:35:00 -0400

    Added functions required.

--- CircularLinkList.py
+++ CircularLinkList.py
@@ -1,1 +1,17 @@
-
+Class Node():
+  def __init__(self,data):
+    self.data=data
+    self.next=None
+    
+Class CircularLinkList():
+  def __init__():
+    self.head=None
+  
+  def append():
+    pass
+  def prepend():
+    pass
+  
+  def print_list():
+    pass
+  
