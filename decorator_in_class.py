b54a44a64a23066aaca4fa310de7a408a4ef47cb
riteshvado <87545202+riteshvado@users.noreply.github.com>
2022-01-06 13:03:11 -0500

    Updated: instance info in decorator function.
    Example update to show how to access instance variables inside of decorator.

--- decorator_in_class.py
+++ decorator_in_class.py
@@ -5,8 +5,14 @@ class A():
     
     ##Standalone function inside. The function can be just outside as well. 
     def _decorate(func):
+        ##See below how decorator is manipulating instance variable . This can be needed in some cases. 
         def wrapper(*args,**kwargs):
+            ##args[0] will get you value self 
+            ##args[1] will get you self
+            
             print(args)
+            ##Getting instance var value 
+            print(args[0].a)
             print(kwargs)
             if kwargs.get('t') == 'egg':
                 kwargs['t']='Omlette'
